0d18ba8d10f87e380c1215f8a1684986705b04ca
jonatancode <jonathan.lp17@gmail.com>
2016-07-28 18:57:33 +0200

    HOla mundo

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var path = require('path');
+
+var routes = require('./routes/index');
+
+var app = express();
+app.use("/", routes);
+
+var port = Number(process.env.PORT || 8080);
+app.listen(port);
